e7c0c4aa3a1abb593916c0eaaad601fe81bc1768
Urvish Joshi <urvishjoshi49@gmail.com>
2019-12-30 22:17:33 +0530

    Delete dbconn.php

--- casino-gaming/user/dbconn.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	$host = "localhost";
-	$db_name = "casinodb";
-	$username = "root";
-	$password = "";
-
-	$conn=mysqli_connect($host,$username,$password,$db_name) or die("Could not connect to db:".mysqli_error($conn));
-
-?>
